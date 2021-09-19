ea7612f0120ab85c173c8ea3b8b881e03fe7a618
Mukesh Mandora <mandoramuku07@gmail.com>
2021-09-19 21:38:32 +0530

    Added Imagedownloader extension

---
+++ NYTimesDemo/Extension/ImageDownloaderEx.swift
@@ -0,0 +1,42 @@
+//
+//  ImageDownloaderEx.swift
+//  NYTimesDemo
+//
+//  Created by mukesh on 19/9/21.
+//
+
+import UIKit
+
+let imageCache = NSCache<AnyObject, AnyObject>()
+
+extension UIImageView {
+    func downloadImageFromURL(_ urlString: String) {
+        image = nil
+
+        // check cache for image first
+        if let cachedImage = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            image = cachedImage
+            return
+        }
+
+        // otherwise fire off a new download
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with: url!, completionHandler: { data, _, error in
+
+            // download hit an error so lets return out
+            if error != nil {
+                print(error ?? "")
+                return
+            }
+
+            DispatchQueue.main.async(execute: {
+                if let downloadedImage = UIImage(data: data!) {
+                    imageCache.setObject(downloadedImage, forKey: urlString as AnyObject)
+
+                    self.image = downloadedImage
+                }
+            })
+
+        }).resume()
+    }
+}
